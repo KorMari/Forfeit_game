8e645fdfa671e31d559417b6f7915a69a0f3195f
KorMari <lezinam@yandex.ru>
2023-02-17 19:45:59 +0400

    bigCard and icon

--- app/src/main/java/com/example/forfeitgame/ExerciseActivity.java
+++ app/src/main/java/com/example/forfeitgame/ExerciseActivity.java
@@ -34,10 +34,8 @@ public class ExerciseActivity extends AppCompatActivity {
         }
 
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // прячем панель навигации
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // прячем панель навигации
                 | View.SYSTEM_UI_FLAG_FULLSCREEN // прячем строку состояния
                 | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
@@ -47,8 +45,9 @@ public class ExerciseActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = MainActivity.newIntent(ExerciseActivity.this);
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(ExerciseActivity.this, new Pair<View, String>(textViewForExercise, "pour"));
-                startActivity(intent, options.toBundle());
+//                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(ExerciseActivity.this, new Pair<View, String>(textViewForExercise, "pour"));
+//                startActivity(intent, options.toBundle());
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/forfeitgame/MainActivity.java
+++ app/src/main/java/com/example/forfeitgame/MainActivity.java
@@ -25,10 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // прячем панель навигации
+        getWindow().getDecorView().setSystemUiVisibility(
+                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // прячем панель навигации
                 | View.SYSTEM_UI_FLAG_FULLSCREEN // прячем строку состояния
                 | View.SYSTEM_UI_FLAG_IMMERSIVE
         );
@@ -41,8 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = ExerciseActivity.newIntent(MainActivity.this);
 
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, new Pair<View, String>(imageViewCard, "pour"));
-                startActivity(intent, options.toBundle());
+//                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(
+//                        MainActivity.this,
+//                        new Pair<View, String>(imageViewCard, "pour"));
+//                startActivity(intent, options.toBundle());
+                startActivity(intent);
 
             }
         });
