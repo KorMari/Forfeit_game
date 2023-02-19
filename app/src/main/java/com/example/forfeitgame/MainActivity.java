a9a8823fa7787a514cd7e931e422b1762ec41dde
l.korsakov <artforme2011@gmail.com>
2023-02-19 11:39:11 +0400

    flip card draft

--- app/src/main/java/com/example/forfeitgame/MainActivity.java
+++ app/src/main/java/com/example/forfeitgame/MainActivity.java
@@ -1,134 +1,142 @@
 package com.example.forfeitgame;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageViewBack;
-    private ImageView imageViewFace;
-    private TextView textViewForExercise;
-
-
-    private AnimatorSet flipOutAnimatorSet;
-    private AnimatorSet flipInAnimationSet;
-
+    private ImageView ImageViewFront;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.card);
+        setContentView(R.layout.common);
         initViews();
-
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // прячем панель навигации
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // прячем строку состояния
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-        );
-
-
+        setupWindow();
 
         imageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-flipCard(MainActivity.this, imageViewBack, imageViewFace);
-
+                Log.d("MainActivity", "onClick imageViewBack");
+                flipCard(MainActivity.this, ImageViewFront, imageViewBack);
             }
         });
 
-        imageViewFace.setOnClickListener(new View.OnClickListener() {
+        ImageViewFront.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-                flipCard(MainActivity.this, imageViewBack, imageViewFace);
-
+                // write log
+                Log.d("MainActivity", "onClick ImageViewFront");
+                flipCard(MainActivity.this, imageViewBack, ImageViewFront);
             }
         });
+    }
 
+    private void setupWindow() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide navigation panel
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide state row
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
     }
 
     private void initViews() {
-        imageViewBack = findViewById(R.id.imageViewBack);
-        imageViewFace = findViewById(R.id.imageViewFace);
-        textViewForExercise = findViewById(R.id.textViewForExercise);
-
+        imageViewBack = findViewById(R.id.back);
+        ImageViewFront = findViewById(R.id.front);
     }
 
     public static Intent newIntent(Context context) {
-
         return new Intent(context, MainActivity.class);
     }
 
-
-
-
-    private void flipCard(Context context, View visibleView, View inVisibleView) {
+    public void flipCard(Context context, View visibleView, View inVisibleView) {
         try {
-            flipOutAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(
+            visibleView.setVisibility(View.VISIBLE);
+            float scale = context.getResources().getDisplayMetrics().density;
+            float cameraDist = 8000 * scale;
+            visibleView.setCameraDistance(cameraDist);
+            inVisibleView.setCameraDistance(cameraDist);
+            AnimatorSet flipOutAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(
                     context,
                     R.animator.flip_out
             );
-
-
             flipOutAnimatorSet.setTarget(inVisibleView);
-            flipInAnimationSet = (AnimatorSet) AnimatorInflater.loadAnimator(
+            AnimatorSet flipInAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(
                     context,
                     R.animator.flip_in
             );
-            flipInAnimationSet.setTarget(visibleView);
-
-            AnimatorSet set = new AnimatorSet();
-            set.playTogether(flipOutAnimatorSet, flipInAnimationSet);
-            set.start();
-
-//            inVisibleView.setVisibility(View.GONE);
-
-            set.addListener(new Animator.AnimatorListener() {
+            flipInAnimatorSet.setTarget(visibleView);
+            flipOutAnimatorSet.start();
+            flipInAnimatorSet.start();
+            flipInAnimatorSet.addListener(new AnimatorListenerAdapter() {
                 @Override
-                public void onAnimationStart(@NonNull Animator animation) {
-
-                }
-
-                @Override
-                public void onAnimationEnd(@NonNull Animator animation) {
+                public void onAnimationEnd(Animator animation) {
                     inVisibleView.setVisibility(View.GONE);
                 }
-
-                @Override
-                public void onAnimationCancel(@NonNull Animator animation) {
-
-                }
-
-                @Override
-                public void onAnimationRepeat(@NonNull Animator animation) {
-
-                }
             });
-} catch (Exception e) {
+        } catch (Exception e) {
             Log.d("MainActivity", e.getMessage());
         }
     }
+
+
+//    private void flipCard(Context context, View visibleView, View inVisibleView) {
+//        try {
+//            flipOutAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(
+//                    context,
+//                    R.animator.flip_out
+//            );
+//
+//
+//            flipOutAnimatorSet.setTarget(inVisibleView);
+//            flipInAnimationSet = (AnimatorSet) AnimatorInflater.loadAnimator(
+//                    context,
+//                    R.animator.flip_in
+//            );
+//            flipInAnimationSet.setTarget(visibleView);
+//
+//            AnimatorSet set = new AnimatorSet();
+//            set.playTogether(flipOutAnimatorSet, flipInAnimationSet);
+//            set.start();
+//
+////            inVisibleView.setVisibility(View.GONE);
+//
+//            set.addListener(new Animator.AnimatorListener() {
+//                @Override
+//                public void onAnimationStart(@NonNull Animator animation) {
+//
+//                }
+//
+//                @Override
+//                public void onAnimationEnd(@NonNull Animator animation) {
+//                    inVisibleView.setVisibility(View.GONE);
+//                }
+//
+//                @Override
+//                public void onAnimationCancel(@NonNull Animator animation) {
+//
+//                }
+//
+//                @Override
+//                public void onAnimationRepeat(@NonNull Animator animation) {
+//
+//                }
+//            });
+//        } catch (Exception e) {
+//            Log.d("MainActivity", e.getMessage());
+//        }
+//    }
 
 }
